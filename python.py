44ab6dd238cebb1df689eacf5447036310a4085d
Anand22357 <80689557+Anand22357@users.noreply.github.com>
2021-03-17 16:58:50 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,8 @@
+class rectangle:
+  def __init__(self,length,breadth):
+    self.length=0;
+    self.breadth=0;
+   def calc(self)
+  return self.length*self.breadth
+ob1=rectangle()
+  print('area of rectangle is ',ob1.calc)
